28a8439703a47bcf5029c0645778dc3c9794ac95
Khalid Jameer <kjameer0@gmail.com>
2023-01-25 22:11:17 -0500

    arrays can be created with indices shown

--- script.js
+++ script.js
@@ -1,17 +1,53 @@
-var Student = /** @class */ (function () {
-    function Student(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.fullName = "".concat(firstName, " ").concat(lastName);
+"use strict";
+function $(id) {
+    return document.getElementById(id);
+}
+const stringToArray = (str) => {
+    let start = 0;
+    let end = str.length;
+    if (str[0] === '[')
+        start++;
+    if (str[end - 1] === ']')
+        end--;
+    return str.slice(start, end).split(',');
+};
+//create an array of list elements
+function strArrayToLiElements(arr) {
+    return arr.map((elem, idx) => {
+        const listElement = document.createElement('li');
+        const arrayElementTag = document.createElement('p');
+        arrayElementTag.textContent = elem;
+        listElement.textContent = String(idx);
+        listElement.appendChild(arrayElementTag);
+        return listElement;
+    });
+}
+function makeArrayButton(butId, inputId, outputId) {
+    try {
+        const button = $(butId);
+        const inputElement = $(inputId);
+        const outputElement = $(outputId);
+        if (button === null)
+            throw new Error('non-button');
+        if (inputElement === null)
+            throw new Error('no input element');
+        if (outputElement === null)
+            throw new Error('no output element');
+        button.addEventListener('click', () => {
+            outputElement.textContent = '';
+            const textArr = stringToArray(inputElement === null || inputElement === void 0 ? void 0 : inputElement.value);
+            const elementArray = strArrayToLiElements(textArr);
+            elementArray.forEach((arrEl) => {
+                outputElement.appendChild(arrEl);
+            });
+        });
     }
-    return Student;
-}());
-var arr = [4, 6, 3, 33, 6];
-var displayArray = arr.map(function (e) {
-    var arrEl = document.createElement('p');
-    arrEl.textContent = String(e);
-    return arrEl;
-});
-displayArray.forEach(function (e) {
-    document.body.appendChild(e);
-});
+    catch (errorId) {
+        let message = 'Unknown Error';
+        if (errorId instanceof Error) {
+            message = errorId.message;
+        }
+        reportError({ message });
+    }
+}
+makeArrayButton('submit-arr', 'array-input', 'array-list');
--- script.ts
+++ script.ts
@@ -1,68 +1,46 @@
-//create a function that takes a button id and
-// adds a event listener corresponding to a particular field
-//take string and turn it into array elements
-function $(id: string){
+function $(id: string) {
   return document.getElementById(id);
 }
 const stringToArray = (str: string): string[] => {
-  let start = 0, end = str.length;
-  if(str[0] === '[') start++;
-  if(str[end-1] === ']') end--;
+  let start = 0;
+  let end = str.length;
+  if (str[0] === '[') start++;
+  if (str[end - 1] === ']') end--;
   return str.slice(start, end).split(',');
 };
 //create an array of list elements
 function strArrayToLiElements(arr: string[]) {
-  return arr.map(elem => {
+  return arr.map((elem, idx) => {
     const listElement = document.createElement('li');
-    listElement.innerText = elem;
+    const arrayElementTag = document.createElement('p');
+    arrayElementTag.textContent = elem;
+    listElement.textContent = String(idx);
+    listElement.appendChild(arrayElementTag);
     return listElement;
-  })
+  });
 }
-function makeArrayButton(butId, inputId, outputId) {
-  const button = $(butId);
+function makeArrayButton(butId: string, inputId: string, outputId: string) {
   try {
-  if(button === null) throw 'oh ras';
-    button.addEventListener('click' , () => {
-      const inputElement = document.getElementById(inputId);
-      const outputElement = document.getElementById(outputId);
-      let textArr = stringToArray(inputElement?.innerText || '');
-      let elementArray = strArrayToLiElements(textArr);
-      if(outputElement) {
-        elementArray.forEach(arrEl => {
-          outputElement.appendChild(arrEl);
-        })
-      }
-    })
-  } catch(errorId) {
-    alert(errorId.message)
+    const button = $(butId);
+    const inputElement = $(inputId) as HTMLInputElement;
+    const outputElement = $(outputId);
+    if (button === null) throw new Error('non-button');
+    if (inputElement === null) throw new Error('no input element');
+    if (outputElement === null) throw new Error('no output element');
+    button.addEventListener('click', () => {
+      outputElement.textContent = '';
+      const textArr = stringToArray(inputElement?.value);
+      const elementArray = strArrayToLiElements(textArr);
+      elementArray.forEach((arrEl) => {
+        outputElement.appendChild(arrEl);
+      });
+    });
+  } catch (errorId: unknown) {
+    let message = 'Unknown Error';
+    if (errorId instanceof Error) {
+      message = errorId.message;
+    }
+    reportError({ message });
   }
 }
-const arr = [4,6,3,33,6];
-const displayArray = arr.map((e : number) => {
-  const arrEl =  document.createElement('p');
-  arrEl.textContent = String(e);
-  return arrEl;
-})
-displayArray.forEach(e => {
-  document.body.appendChild(e);
-})
-//write function that takes array and returns array of html elements
- /*what kinds of elements
- okay i think i should be able to do:
- 1. pop
- 2. push
- 3. length
- im thinking i should just do horizontal divs maybe
- i wanna keep the array elements in the same line
- i want a form to decide what to put in the array elements wise
- i could do:
- 1. horizontal li elements
- 2. horizontal divs
-
- */
- let temp = Array.from(document.getElementsByTagName('script')).find(e => {
-  let text = e.innerText;
-  const regex = /hideArticle/
-  return regex.test(text) === true
-})
-console.log(temp)
+makeArrayButton('submit-arr', 'array-input', 'array-list');
